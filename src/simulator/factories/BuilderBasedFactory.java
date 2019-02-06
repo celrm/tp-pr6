1d68b658cd822465b1f5dd153bbe7eddf7e6ab3b
María Arranz Lobo <mariaarranzlobo@eduroam235013.eduroam.ucm.es>
2019-02-07 00:18:30 +0100

    mas mierda inutil

---
+++ src/simulator/factories/BuilderBasedFactory.java
@@ -0,0 +1,24 @@
+package simulator.factories;
+
+import java.util.List;
+
+import org.json.JSONObject;
+
+public class BuilderBasedFactory<T> implements Factory<T>{
+	
+	public BuilderBasedFactory(List<Builder<T>> builders){
+		
+	}
+	@Override
+	public T createInstance(JSONObject info) throws IllegalArgumentException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<JSONObject> getInfo() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
